d9bee9d00dbc001c87aae99fb65ceed6221ff9ad
uzi3 <43525327+uzi3@users.noreply.github.com>
2021-01-16 10:01:23 +0530

    first commit

---
+++ Remote Registry v2.0.ps1
@@ -0,0 +1,36 @@
+﻿#Requires Powershell 5.0
+
+$ErrorActionPreference = "Stop"
+
+Class VersionCheck
+{
+$MacNam
+$Value
+}
+
+$Machines = Get-Content D:\MachineList.txt
+
+foreach ($machinename in $Machines)
+{
+$Object=New-Object VersionCheck
+$Object.MacNam = $machinename
+$key = "SYSTEM\\CurrentControlSet\\Services\\NTDS\Parameters"
+$valuename = "Strict Replication Consistency"
+
+Try
+{
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $machinename)
+}
+
+Catch
+{
+$Object.Value = "Error"
+$Object | export-csv D:\SRC.csv -Append -NoTypeInformation
+continue
+}
+
+$regkey = $reg.opensubkey($key)
+$object.Value = $regkey.getvalue($valuename)
+$Object | export-csv D:\SRC.csv -Append -NoTypeInformation
+Clear-Variable Object
+}
